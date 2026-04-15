5730dcc2b1b19aab124e40d73c458683dca63d24
leomessi-l <lorenzocesarmorales@gmail.com>
2026-04-15 14:41:45 -0300

    Update validar.php

--- validar.php
+++ validar.php
@@ -1,1 +1,29 @@
+<?php
+// Establecer la conexión a la base de datos
+$servername = "localhost";
+$username = "tu_usuario"; // Cambiar por tu nombre de usuario
+$password = "tu_contraseña"; // Cambiar por tu contraseña
+$dbname = "baseddatololoog"; // Cambiar por el nombre de tu base de datos
 
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+die("Conexión fallida: " . $conn->connect_error);
+}
+// Obtener los datos del formulario
+$usuario = $_POST['usuario'];
+$contrasena = $_POST['contrasena'];
+
+// Consultar la base de datos
+$sql = "SELECT * FROM log in WHERE usuario='$usuario' AND password ='$contrasena'";
+$result = $conn->query($sql);
+
+if ($result->num_rows == 1) {
+// Inicio de sesión exitoso
+echo "Inicio de sesión exitoso. Bienvenido, $usuario.";
+} else {
+// Error en las credenciales
+echo "Usuario o contraseña incorrectos. Por favor, inténtalo de nuevo.";
+}
+$conn->close();
+?>
